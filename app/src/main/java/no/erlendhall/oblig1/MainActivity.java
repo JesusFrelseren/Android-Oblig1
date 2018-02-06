2654e94ee15a97859e72b8da86abd5b1a8fb4595
JesusFrelseren <1tdwsaotsoraD>
2018-02-06 11:29:58 +0100

    spinner listener

--- app/src/main/java/no/erlendhall/oblig1/MainActivity.java
+++ app/src/main/java/no/erlendhall/oblig1/MainActivity.java
@@ -1,26 +1,50 @@
 package no.erlendhall.oblig1;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+    Spinner spinner;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initSpinner();
+        spinner.setOnItemSelectedListener(this);
+
+    }
+
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+
+        //funksjonalitet
+        TextView test = findViewById(R.id.test);
+
+        Context context = getApplicationContext();
+        CharSequence text = "Hello toast!";
+        int duration = Toast.LENGTH_SHORT;
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void onNothingSelected(AdapterView<?> parent) {
 
     }
 
     protected void initSpinner() {
-        Spinner spinner = findViewById(R.id.spin_countries);
+        spinner = findViewById(R.id.spin_countries);
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
                 this, R.array.countries, android.R.layout.simple_spinner_item);
 
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
         spinner.setAdapter(adapter);
+
     }
 }
