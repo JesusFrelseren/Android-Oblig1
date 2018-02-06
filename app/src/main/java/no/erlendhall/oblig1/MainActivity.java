daf255ca6a7676dc8fa1c21f45ac60f53652e1f3
JesusFrelseren <1tdwsaotsoraD>
2018-02-06 10:37:11 +0100

    spinner init

--- app/src/main/java/no/erlendhall/oblig1/MainActivity.java
+++ app/src/main/java/no/erlendhall/oblig1/MainActivity.java
@@ -2,6 +2,8 @@ package no.erlendhall.oblig1;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initSpinner();
+
+    }
+
+    protected void initSpinner() {
+        Spinner spinner = findViewById(R.id.spin_countries);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.countries, android.R.layout.simple_spinner_item);
+
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+        spinner.setAdapter(adapter);
     }
 }
